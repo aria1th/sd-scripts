407b1a134a418e73f5e7080760035cf9baac9161
AngelBottomless <aria1th@naver.com>
2025-05-25 04:12:30 +0900

    Update sdxl_train.py

--- sdxl_train.py
+++ sdxl_train.py
@@ -622,6 +622,8 @@ def train(args):
                     if args.masked_loss:
                         loss = apply_masked_loss(loss, batch)
                     loss = loss.mean([1, 2, 3])
+                    loss_weights = batch["loss_weights"]  # 各sampleごとのweight
+                    loss = loss * loss_weights
                     if args.fourier_loss_weight > 0:
                         freq_loss = fourier_highfreq_loss(noise_pred.float(), target.float(), reduction="none")
                         loss = loss + args.fourier_loss_weight * freq_loss
